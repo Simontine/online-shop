13c2ac2b2a7c789d494e89392daf7980e72904d3
Isaac Malebana <isaac.malebana@dadevs.co.za>
2022-02-02 10:38:34 +0200

    checkout form with validation

--- Frontend/src/app/components/checkout/checkout.component.ts
+++ Frontend/src/app/components/checkout/checkout.component.ts
@@ -1,34 +1,76 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+import { SharedDataService } from 'src/app/services/shared-data.service';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.css']
+  styleUrls: ['./checkout.component.css'],
 })
 export class CheckoutComponent implements OnInit {
+  constructor(private fb: FormBuilder, private sharedDataService: SharedDataService) {}
 
-  constructor(private fb: FormBuilder) { }
+  productInCart: any;
+  totalInCart: number  = 0;
+
 
   ngOnInit(): void {
+ 
+    if (localStorage.getItem('Products')) {
+      let localProduct: any = localStorage.getItem('Products');
+      this.productInCart = JSON.parse(localProduct);
+
+      this.productInCart.forEach((data: any) => {
+        this.totalInCart += data.subtotal;
+        console.log(this.totalInCart);
+      });
+      this.sharedDataService.changeMessage(this.productInCart.length);
+    }
   }
 
   checkoutForm = this.fb.group({
-    firstname: [''],
-    lastname: [''],
-    email: [''],
-    address: [''],
-    city: [''],
-    province: [''],
-    phoneNumber: ['']
-  })
+    firstname: ['', Validators.required],
+    lastname: ['', Validators.required],
+    email: ['', Validators.required],
+    city: ['', Validators.required],
+    province: ['', Validators.required],
+    address: ['', Validators.required],
+    phoneNumber: ['', Validators.required],
+  });
 
-  submit(){
-    console.log("Hass been clicked")
+  get firstname() {
+    return this.checkoutForm.get('firstname');
   }
 
-  form(){
-    console.log(this.checkoutForm.value)
+  get lastname() {
+    return this.checkoutForm.get('lastname');
   }
 
+  get email() {
+    return this.checkoutForm.get('email');
+  }
+
+  get city() {
+    return this.checkoutForm.get('city');
+  }
+
+  get province() {
+    return this.checkoutForm.get('province');
+  }
+
+  get address() {
+    return this.checkoutForm.get('address');
+  }
+
+  get phoneNumber() {
+    return this.checkoutForm.get('phoneNumber');
+  }
+
+  submit() {
+    console.log('Hass been clicked');
+  }
+
+  form() {
+    console.log(this.checkoutForm.value);
+  }
 }
